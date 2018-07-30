95d2ba3883031bace6be806c416db7f4b5c418ca
Themaker482 <carmonamendozabernabe@gmail.com>
2018-07-30 07:34:39 -0500

    Update config.py

--- appodeal/config.py
+++ appodeal/config.py
@@ -1,4 +1,4 @@
-def can_build(env, plat):
+def can_build(plat):
 	return plat=="android"
 
 def configure(env):
@@ -9,3 +9,5 @@ def configure(env):
 		env.android_add_dependency("compile 'com.google.android.gms:play-services-location:10.2.1'")
 		env.android_add_dependency("compile fileTree(dir: '../../../modules/appodeal/android/libs', include: '*.jar')")
 		env.android_add_dependency("compile fileTree(dir: '../../../modules/appodeal/android/libs', include: '*.aar')")
+
+		
